490ea6ceebc0140461047b75f4633eb752405978
Terence Onyekachi <87383877+Terencino8@users.noreply.github.com>
2024-02-10 10:17:00 +0100

    almost done it remains animation

--- src/home/Footer.jsx
+++ src/home/Footer.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <p>Courtesy: Terence Onyekachi</p>
+
+    </div>
   )
 }
 
--- src/home/Homesection4.jsx
+++ src/home/Homesection4.jsx
@@ -12,10 +12,41 @@ const Homesection4 = () => {
         <div className="right">
           <div className="rightwrapper">
           <h1 className="fourhead">
-
+            From Your Children 
+            <hr />
           </h1>
           <p className="fourdesc">
-            
+              Dear Mom and Dad,
+              <br />
+              On this special occasion of your 25th wedding anniversary, we, your five children, want to express our deepest gratitude and love for everything you have done for us and for each other over the years.
+              <br />
+              Mom and Dad, your love story has been an inspiration to us all. Your unwavering commitment to each other, your patience, your sacrifices, and your enduring love have shaped not only your lives but ours as well. You have shown us what it means to build a strong and loving partnership, and for that, we are eternally grateful.
+              <br />
+              As we celebrate this remarkable milestone in your journey together, we want you to know how much you mean to us. Your love and support have been the foundation upon which we have built our lives and our dreams. Your guidance and wisdom have been our compass in navigating life's challenges.
+              <br />
+              Today, as we raise our glasses to toast to 25 years of your beautiful marriage, we want to say thank you. Thank you for being the most amazing parents any child could ask for. Thank you for your endless love, your unwavering support, and your constant encouragement.
+              <br />
+              May your love continue to grow stronger with each passing day. May your hearts remain forever intertwined, and may your bond only deepen as you continue this incredible journey together.
+              <br />
+              Happy 25th anniversary, Mom and Dad! Here's to many more years of love, laughter, and cherished memories.
+              <br />
+              With all our love,
+              <br />
+              Terence
+              <br />
+              Felicitas
+              <br />
+              Dominic 
+              <br />
+              Augustine 
+              <br />
+              Mario-Pascal
+              <br />
+              Cheers! 🥂
+              <br />
+
+    
+
           </p>
 
           </div>
